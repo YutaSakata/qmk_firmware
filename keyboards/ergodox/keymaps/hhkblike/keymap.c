0f3ba2c28a1e1aa7ab8db2a30948217fad40d9ad
Yuta Sakata <yusaka39@gmail.com>
2016-09-07 19:22:51 +0900

    Fix syntax

--- keyboards/ergodox/keymaps/hhkblike/keymap.c
+++ keyboards/ergodox/keymaps/hhkblike/keymap.c
@@ -36,7 +36,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_EQL,        KC_1,        KC_2,   KC_3,   KC_4,   KC_5,   KC_GRV,
         KC_TAB,        KC_Q,        KC_W,   KC_E,   KC_R,   KC_T,   TG(SYMB),
         KC_BSPC,       KC_A,        KC_S,   KC_D,   KC_F,   KC_G,
-        KC_LSHIFT      KC_Z,        KC_X,   KC_C,   KC_V,   KC_B,   TG(BASE),
+        KC_LSHIFT,     KC_Z,        KC_X,   KC_C,   KC_V,   KC_B,   TG(BASE),
         KC_FN2,        KC_LGUI,     KC_ESC, KC_LALT,KC_LCTL,
                                                       RESET,  KC_LEFT,
                                                               KC_RIGHT,
